a005fd31984755861a2c018076caec2893f4f1f2
Matt Reed <mattreed@fastmail.com>
2023-08-09 15:10:08 -0600

    resuming no longer resets timer and earned money

--- script.js
+++ script.js
@@ -18,26 +18,50 @@ function startTimer() {
         return;
     }
 
-    startTime = Date.now();
-    timerInterval = setInterval(updateTimer, 1000);
-    startButton.disabled = true;
-    pauseButton.disabled = false;
+        // Retrieve previously saved values from local storage
+        const savedTime = parseFloat(localStorage.getItem('elapsedTime')) || 0;
+        const savedEarnedMoney = parseFloat(localStorage.getItem('earnedMoney')) || 0;
+    
+        startTime = Date.now() - savedTime * 1000;
+        timerInterval = setInterval(updateTimer, 1000);
+        startButton.disabled = true;
+        pauseButton.disabled = false;
+    
+        // Set the previous earned money value
+        earnedMoneyElement.textContent = '$' + savedEarnedMoney.toFixed(2);
+
 }
 
 function pauseTimer() {
     clearInterval(timerInterval);
     startButton.disabled = false;
     pauseButton.disabled = true;
+    startButton.innerText = 'Resume';
+
+    //save the current elapsed time so that it can be the starting elapsed time when the timer is resumed
+    const currentTime = Date.now();
+    const elapsedTime = (currentTime - startTime) / 1000; // in seconds
+    localStorage.setItem('elapsedTimeAccumulated', elapsedTime);
+
+
 }
 
 function updateTimer() {
+
     const currentTime = Date.now();
     const elapsedTime = (currentTime - startTime) / 1000; // in seconds
     const earnedMoney = (elapsedTime / 3600) * payRate; // hourly rate
 
     timeElapsedElement.textContent = formatTime(elapsedTime);
     earnedMoneyElement.textContent = '$' + earnedMoney.toFixed(2);
+
+    // Save current elapsed time and earned money to local storage
+    localStorage.setItem('elapsedTime', elapsedTime);
+    localStorage.setItem('earnedMoney', earnedMoney);
+
 }
+
+
 
 function formatTime(seconds) {
     const hours = Math.floor(seconds / 3600);
